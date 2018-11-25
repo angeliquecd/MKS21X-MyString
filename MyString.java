dd89247c7d18bcef4bc1c33247e762679cee5a19
angeliquecd <acharles-davis00@stuy.edu>
2018-11-25 07:21:04 -0500

    added comments

--- MyString.java
+++ MyString.java
@@ -1,18 +1,18 @@
 public class MyString implements CharSequence, Comparable<CharSequence> {
   private char[] data;
-public MyString(CharSequence a){
+public MyString(CharSequence a){//places each value into slot in array
   data= new char[a.length()];
   for (int b=0;b<a.length();b++){
     data[b]=a.charAt(b);
   }
 }
-public char charAt(int index){
+public char charAt(int index){//returns certain array value
   return data[index];
 }
-public int length(){
+public int length(){//finds length of array
   return data.length;
 }
-public String subSequence(int start, int end){
+public String subSequence(int start, int end){//loops through only part of data
   String value="";
   for (int a = start;a<end;a++){
     value+=""+data[a];
@@ -21,16 +21,16 @@ public String subSequence(int start, int end){
 }
 public String toString(){
   String value ="";
-  for (int a =0;a<data.length;a++){
+  for (int a =0;a<data.length;a++){//loops through data and adds to print statement
     value+=""+data[a];
   }
   return value;
 }
 public int compareTo(CharSequence b){
-  MyString b2 = new MyString(b);
-  return this.toString().compareTo(b2.toString());
+  MyString b2 = new MyString(b);//converts other vaue to mystring
+  return this.toString().compareTo(b2.toString());//converts both to strings and then compares them
 }
-  public static void main (String args[]){
+  public static void main (String args[]){//test cases
 MyString guy = new MyString("hello");
 MyString bro = new MyString("i am but a chimney sweep.");
 System.out.println(guy);
